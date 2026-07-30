b9a1351a7320affe738f53a7a7ba3c009453ca17
Cebisile16 <ngubsceby@gmail.com>
2026-07-30 15:56:27 +0000

    Refact: The JS was moved into the js file

---
+++ Javascript/app.js
@@ -0,0 +1,12 @@
+function changeCity() {
+    let city = prompt("Enter a city name:");
+
+    if (city) {
+        document.getElementById("city-heading").innerHTML =
+            `Currently 21° in ${city}`;
+    }
+}
+
+const changeCityButton = document.querySelector(".change-city");
+
+changeCityButton.addEventListener("click", changeCity);
