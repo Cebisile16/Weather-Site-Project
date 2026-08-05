f7304a301738cb77b2a940c1ae14fca7d778fac8
Cebisile16 <ngubsceby@gmail.com>
2026-08-05 20:38:36 +0000

    ADD: Added the API with the APIKey

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,13 +1,25 @@
-function changeCity() {
-    let city = prompt("Enter a city name:");
+const apiKey = c09e9858e678b141e53b28da37da62da;
 
-    if (city) {
-        document.getElementById("city-heading").innerHTML =
-            `Currently 21° in ${city}`;
-    }
-}
+const button = document.getElementById("searchBtn");
 
-const changeCityButton = document.querySelector(".change-city");
+button.addEventListener("click", () => {
 
-changeCityButton.addEventListener("click", changeCity);
-console.log("Weather App Loaded!");
+    const city = document.getElementById("cityInput").value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+
+            document.getElementById("weatherResult").innerHTML = `
+                <h2>${data.name}</h2>
+                <p>Temperature: ${data.main.temp} °C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+});
