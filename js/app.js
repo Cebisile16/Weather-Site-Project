1cd669ed875dc8b4869350c7ef72e8f3b190ba89
Cebisile16 <ngubsceby@gmail.com>
2026-08-19 21:53:23 +0200

    REFACTOR: reorganize weather app structure

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const apiKey = "smdnskdmsdmsl";
+
+const button = document.getElementById("searchBtn");
+
+button.addEventListener("click", () => {
+  const city = document.getElementById("cityInput").value;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("weatherResult").innerHTML = `
+                <h2>${data.name}</h2>
+                <p>Temperature: ${data.main.temp} °C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
